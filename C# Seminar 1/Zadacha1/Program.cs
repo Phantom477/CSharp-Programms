b187f6f684782e67e2bc69c114382a12d3b4a4d0
Ruslan Galikeev <131903934+Phantom477@users.noreply.github.com>
2023-05-20 16:53:55 +0300

    CSharpSeminar1

---
+++ C# Seminar 1/Zadacha1/Program.cs
@@ -0,0 +1,63 @@
+﻿// Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да 
+// a = 2, b = 10 -> нет 
+// a = 9, b = -3 -> да 
+// a = -3 b = 9 -> нет
+
+// Console.Write("Введите целое число A: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите целое число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+//     int resultA = numberA * numberA;
+// int resultB = numberB * numberB;
+// if (numberA == resultB)
+//     System.Console.WriteLine("Число А является квадратом числа В = ДА");
+// else
+//     System.Console.WriteLine("Число А является квадратом числа В = НЕТ");
+
+
+// Задача №3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+// Console.Write("Введите день недели: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// switch (num)
+// {
+//     case 1:
+//         System.Console.WriteLine("Понедельник");
+//         break;
+//     case 2: 
+//         System.Console.WriteLine("Вторник");
+//         break;
+//     case 3:
+//         System.Console.WriteLine("Среда");
+//         break;
+//     case 4:
+//         System.Console.WriteLine("Четверг");
+//         break;
+// }
+
+// Console.Write("Введите день недели: "); 
+// int num = Convert.ToInt32(Console.ReadLine()); 
+// if (num == 1) 
+//     System.Console.WriteLine("Понедельник"); 
+// else if (num == 2) 
+//     System.Console.WriteLine("Вторник"); 
+// else if (num == 3) 
+//     System.Console.WriteLine("Среда"); 
+// else if (num == 4) 
+//     System.Console.WriteLine("Четверг"); 
+// else if (num == 5) 
+//     System.Console.WriteLine("Пятница"); 
+// else if (num == 6) 
+//     System.Console.WriteLine("Суббота"); 
+// else if (num == 7) 
+//     System.Console.WriteLine("Воскресенье"); 
+// else  
+//     System.Console.WriteLine("Вы неправильно ввели число");
+
+// Console.WriteLine("Введите целое число");
+// int num = Convert.ToInt32 (Console.ReadLine());
+// string[] array = {"Понедельник", "Вторник"};
+// System.Console.WriteLine(array[num-1]);
